a74154bb9b2b6c5d631aa57d97d071bc74336094
Programmer Muslim <rizki@imastudio.co.id>
2017-11-14 23:20:15 +0700

    fix bug

--- AplikasiBeritaiOS/AplikasiBeritaiOS/BeritaKategoriTableViewController.swift
+++ AplikasiBeritaiOS/AplikasiBeritaiOS/BeritaKategoriTableViewController.swift
@@ -7,21 +7,51 @@
 //
 
 import UIKit
+import AlamofireImage
+import Alamofire
+import SwiftyJSON
 
 class BeritaKategoriTableViewController: UITableViewController {
     //deklarasi variable untuk menampung id yang di kirim
     var nampungId : String? = nil
+    var arrayberita = [[String:String]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("id + " + nampungId!)
+        
+        let params = ["first_name" : nampungId!]
+         let url = "http://localhost/ServerBerita/index.php/api/getBeritaByIdKategori"
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        Alamofire.request(url, method: .post, parameters: params, encoding: URLEncoding.default, headers: nil).responseJSON(completionHandler: { (response) in
+            
+            //check response
+            if response.result.isSuccess {
+                
+                //kalau response success kita ambil json
+                let json = JSON(response.result.value as Any)
+                
+                print(json)
+                //get jsonarray dari json diatas
+                self.arrayberita = json["data"].arrayObject as! [[String : String]]
+                //check di log
+                // print(self.arrayberita)
+                
+                //check jumlah array
+                if(self.arrayberita.count > 0){
+                    
+                    //refresh tableview
+                    self.tableView.reloadData()
+                }
+            }
+            else{
+                
+                print("error server")
+                
+            }
+        })
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,23 +63,59 @@ class BeritaKategoriTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return arrayberita.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellBeritaKategori", for: indexPath) as! BeritaKategoriTableViewCell
 
-        // Configure the cell...
+        var serverid = arrayberita[indexPath.row]
+        
+        //print(serverid)
+        
+        var id =  serverid["id_berita"]
+        let judul = serverid["judul"]
+        let isiBerita = serverid["isi_berita"]
+        // print(judul)
+        var gambar = serverid["gambar"]
+        
+        //pindahkan ke label
+        cell.labelJudul.text = judul
+        cell.labelIsi.text = isiBerita
+        
+        //download gambar dari server
+        
+        //download gambar dari server
+        Alamofire.request("http://localhost/ServerBerita/foto_berita/"+gambar!).responseImage { (datagambar) in
+            
+            //check response
+            if datagambar.result.isSuccess {
+                
+                //ambil gambar yang udah dikirim server
+                let gambarserver = datagambar.result.value
+                
+                //tempelin ke image di cell tableview
+                cell.imgBerita.image = gambarserver
+                
+            }
+                //if error
+            else{
+                
+            }
+        }
+        
+        
+        
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
