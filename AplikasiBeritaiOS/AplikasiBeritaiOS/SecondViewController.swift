48f1635b3029780d9081893a1fdac4bbd1ffd03e
Programmer Muslim <rizki@imastudio.co.id>
2017-11-14 11:01:43 +0700

    add project

---
+++ AplikasiBeritaiOS/AplikasiBeritaiOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AplikasiBeritaiOS
+//
+//  Created by Rizki Syaputra on 11/14/17.
+//  Copyright © 2017 Rizki Syaputra. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
